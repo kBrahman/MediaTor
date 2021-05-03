6ee17b6e3d08b578b54ef9e585cf4b6b773c5e36
Kairat Doshekenov <kayrat@bk.ru>
2021-05-03 08:15:47 +0600

    update

--- src/z/zer/tor/media/android/gui/views/KeywordFilterDrawerView.java
+++ src/z/zer/tor/media/android/gui/views/KeywordFilterDrawerView.java
@@ -1,26 +1,7 @@
-/*
- * Created by Angel Leon (@gubatron), Alden Torres (aldenml),
- *            Marcelina Knitter (@marcelinkaaa)
- * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package z.zer.tor.media.android.gui.views;
 
 import android.app.Activity;
 import android.content.Context;
-import androidx.annotation.Nullable;
 import android.text.Editable;
 import android.text.Html;
 import android.text.TextWatcher;
@@ -37,12 +18,7 @@ import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import z.zer.tor.media.R;
-import z.zer.tor.media.android.core.ConfigurationManager;
-import z.zer.tor.media.android.core.Constants;
-import z.zer.tor.media.android.gui.util.UIUtils;
-import z.zer.tor.media.search.KeywordDetector;
-import z.zer.tor.media.search.KeywordFilter;
+import androidx.annotation.Nullable;
 
 import java.util.Collections;
 import java.util.EnumMap;
@@ -50,11 +26,13 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
 
-/**
- * @author aldenml
- * @author gubatron
- * @author marcelinkaaa
- */
+import z.zer.tor.media.R;
+import z.zer.tor.media.android.core.ConfigurationManager;
+import z.zer.tor.media.android.core.Constants;
+import z.zer.tor.media.android.gui.util.UIUtils;
+import z.zer.tor.media.search.KeywordDetector;
+import z.zer.tor.media.search.KeywordFilter;
+
 public final class KeywordFilterDrawerView extends LinearLayout {
 
     private KeywordFiltersPipelineListener pipelineListener;
