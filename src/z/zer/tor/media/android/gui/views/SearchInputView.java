6e60d274a0c5c901836386da29f861aa45582924
Kairat Doshekenov <kayrat@bk.ru>
2021-08-20 09:45:07 +0600

    remove apps tab

--- src/z/zer/tor/media/android/gui/views/SearchInputView.java
+++ src/z/zer/tor/media/android/gui/views/SearchInputView.java
@@ -1,25 +1,9 @@
-/*
- * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package z.zer.tor.media.android.gui.views;
 
 import android.content.Context;
+
 import com.google.android.material.tabs.TabLayout;
+
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.SparseArray;
@@ -46,7 +30,7 @@ public class SearchInputView extends LinearLayout {
     private final SparseArray<FileTypeTab> toFileTypeTab;
 
     private enum FileTypeTab {
-        TAB_AUDIO(Constants.FILE_TYPE_AUDIO, 0), TAB_VIDEOS(Constants.FILE_TYPE_VIDEOS, 1), TAB_PICTURES(Constants.FILE_TYPE_PICTURES, 2), TAB_APPLICATIONS(Constants.FILE_TYPE_APPLICATIONS, 3), TAB_DOCUMENTS(Constants.FILE_TYPE_DOCUMENTS, 4), TAB_TORRENTS(Constants.FILE_TYPE_TORRENTS, 5);
+        TAB_AUDIO(Constants.FILE_TYPE_AUDIO, 0), TAB_PICTURES(Constants.FILE_TYPE_PICTURES, 2), TAB_APPLICATIONS(Constants.FILE_TYPE_APPLICATIONS, 3), TAB_DOCUMENTS(Constants.FILE_TYPE_DOCUMENTS, 4), TAB_TORRENTS(Constants.FILE_TYPE_TORRENTS, 5);
 
         final byte fileType;
         final int position;
@@ -68,10 +52,7 @@ public class SearchInputView extends LinearLayout {
         this.adapter = new SuggestionsAdapter(context);
         toFileTypeTab = new SparseArray<>();
         toFileTypeTab.put(Constants.FILE_TYPE_AUDIO, FileTypeTab.TAB_AUDIO);
-        toFileTypeTab.put(Constants.FILE_TYPE_VIDEOS, FileTypeTab.TAB_VIDEOS);
         toFileTypeTab.put(Constants.FILE_TYPE_PICTURES, FileTypeTab.TAB_PICTURES);
-        toFileTypeTab.put(Constants.FILE_TYPE_APPLICATIONS, FileTypeTab.TAB_APPLICATIONS);
-        toFileTypeTab.put(Constants.FILE_TYPE_DOCUMENTS, FileTypeTab.TAB_DOCUMENTS);
         toFileTypeTab.put(Constants.FILE_TYPE_TORRENTS, FileTypeTab.TAB_TORRENTS);
     }
 
